57624a642e790a417373997db9f4fd4d496fc59e
huybrechtsarthur <huybrechts.arthur@gmail.com>
2022-08-23 15:59:37 +0200

    Gatsby Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to Huybrechts Agency">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="Oh no"
+        src="../images/gurapog.png"
+      />
     </Layout>
   )
 }
